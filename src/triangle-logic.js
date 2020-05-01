2a00d07d616203243181043a971991a9e63fbfb4
Soro Cat <sorousha326@gmail.com>
2020-05-01 09:11:23 -0700

    new changes

--- src/triangle-logic.js
+++ src/triangle-logic.js
@@ -7,8 +7,8 @@ export function Triangle(side1, side2, side3){
 }
 
   Triangle.prototype.Typecheck = function();
-    if ((this.side1 > (this.side2 > this.side3))) || ((this.side3 > (this.side2 > this.side1))) {
-      if ((this.side2 > (this.side3 > this.side1))) || ((this.side1 > (this.side3 > this.side2))) {
+    if ((this.side1 > (this.side2 > this.side3))) || ((this.side3 > (this.side2 > this.side1))) || ((this.side2 > (this.side3 > this.side1))) || ((this.side1 > (this.side3 > this.side2))) {
+      return "not a triangle"
 
       }
     }
