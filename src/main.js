0eb425c96fd15920b15b845318368ffaeec373ce
Soro Cat <sorousha326@gmail.com>
2020-05-01 11:09:31 -0700

    fix display and labeling in js

--- src/main.js
+++ src/main.js
@@ -2,15 +2,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import {Triangle} from "./triangle-logic.js";
 
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
     var Angle1 = parseInt($("input#AngleOne").val());
     var Angle2 = parseInt($("input#AngleTwo").val());
     var Angle3 = parseInt($("input#AngleThree").val());
+    var newTriangle = new Triangle(Angle1, Angle2, Angle3);
     var sum = Angle1+Angle2+Angle3;
-    console.log(Angle1);
+    console.log(newTriangle, "newTriangle");
     $("#output").text(sum);
+
+    $("#type").text(newTriangle.Typecheck());
     // if (Angle1&&Angle2)(Angle2&&Angle3)(Angle3&&Angle1) {
     //
     // }
--- src/triangle-logic.js
+++ src/triangle-logic.js
@@ -3,20 +3,14 @@ export function Triangle(side1, side2, side3){
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
-  debugger;
 }
 
-  Triangle.prototype.Typecheck = function();
-    if ((this.side1 > (this.side2 > this.side3))) || ((this.side3 > (this.side2 > this.side1))) || ((this.side2 > (this.side3 > this.side1))) || ((this.side1 > (this.side3 > this.side2))) {
-      return "not a triangle"
-
-      {  else if ((this.side1 > (this.side2 > this.side3))) || ((this.side3 > (this.side2 > this.side1))) || ((this.side2 > (this.side3 > this.side1))) || ((this.side1 > (this.side3 > this.side2))) {
-         return "isoceles"
-      }
-
-      {  else if ((this.side1 > (this.side2 > this.side3))) || ((this.side3 > (this.side2 > this.side1))) || ((this.side2 > (this.side3 > this.side1))) || ((this.side1 > (this.side3 > this.side2))) {
-          return "scalene"
-      }
-
-      }
+  Triangle.prototype.Typecheck = function(){
+    if ((this.side1 > (this.side2 > this.side3)) || (this.side3 > (this.side2 > this.side1)) || (this.side2 > (this.side3 > this.side1)) || (this.side1 > (this.side3 > this.side2))) {
+      return "not a triangle";
+     }  else if ((this.side1 > (this.side2 > this.side3)) || (this.side3 > (this.side2 > this.side1)) || (this.side2 > (this.side3 > this.side1)) || (this.side1 > (this.side3 > this.side2))) {
+         return "isoceles";
+      }  else if ((this.side1 > (this.side2 > this.side3)) || (this.side3 > (this.side2 > this.side1)) || (this.side2 > (this.side3 > this.side1)) || (this.side1 > (this.side3 > this.side2))) {
+          return "scalene";
     }
+  }
